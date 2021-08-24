8157bac54edc4dc116602c3b827f8601a644f15b
anilraj <anil050797@gmial.com>
2021-08-24 17:35:01 +0530

    index

--- server/index.js
+++ server/index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
 const route = express.Router();
-
 route.get('/', (req, res)=>{
     res.status(200).json('hello form the server side')
 });
 app.use('/about', route);
-
-S
-const url = 'mongodb+srv://anilraj:<password>@cluster0.kk74h.mongodb.net/test?retryWrites=true&w=majority'
-
+// const url = 'mongodb+srv://anilraj:anilraj5@cluster0.kk74h.mongodb.net/mern180821?retryWrites=true&w=majority'
+const url = 'mongodb+srv://anilraj:anilraj5@cluster0.wbrda.mongodb.net/mern270721?retryWrites=true&w=majority'
 mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -22,11 +18,7 @@ mongoose.connect(url, {
 }).catch(err => {
     console.log("Error: ", err.message)
 })
-
-
 const port = 4000;
-
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
-
 })
